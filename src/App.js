d05cc2787a69c2b511c29a099ad95a2384b8cd11
kkkmin <moonkangmin5960@gmail.com>
2024-05-11 05:01:14 +0900

    ddddddddddddssss

--- src/App.js
+++ src/App.js
@@ -1,39 +1,80 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import React, { useEffect, useState } from 'react';
-import LoginPage from './loginpage';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-
-
-function App() {
-  const [loginState, setLoginState] = useState('Login'); 
-  const [logurlState, setLogurlState]= useState('/login');
-
+function Posts({ post }) {
   return (
-    <div className="App">
-  
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="/home">SAMSAMOO</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/funding">Funding</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href={logurlState}>{loginState}</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-    <Routes>
-        <Route path='/login' element={<LoginPage setLoginState={setLoginState} setLogurlState={setLogurlState}/>} />
-    </Routes>  
-
-    </div>
+    <Card style={{ width: '18rem' }} className='Card'>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{post.name}</Card.Title>
+        <Card.Text>
+          {post.data}
+        </Card.Text>
+        <Link to={`/detail/${post.id}`}>
+          <Button variant="primary">자세히 보기</Button>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 }
 
-export default App;
+function Mainpage() {
+  // NewPosts와 RecommendPosts를 useState 내부에서 초기화
+  const [NewPosts, SetNewPosts] = useState([]);
+  const [RecommendPosts, SetRecommendPosts] = useState([]);
+
+  useEffect(() => {
+    // data.js에서 데이터 가져와서 상태 업데이트
+    fetch('url')
+      .then(res => res.json()) // .json() 메서드 호출
+      .then(data => {
+        SetNewPosts(data); // 첫 번째 then 메서드에서 처리
+        SetRecommendPosts(data); // 첫 번째 then 메서드에서 처리
+      });
+  }, []); // 컴포넌트가 처음 렌더링될 때 한 번만 실행됨
+  
+
+
+  let MainNewPosts = [];
+  let MainRecommendPosts = [];
+
+  [0, 1, 2, 3].forEach(index => {
+    if (NewPosts[index]) {
+      MainNewPosts.push(NewPosts[index]);
+    }
+  });
+
+  [7, 6, 5, 4].forEach(index => {
+    if (RecommendPosts[index]) {
+      MainRecommendPosts.push(RecommendPosts[index]);
+    }
+  });
+
+
+
+
+  return (
+    <>
+      <div className='Posts'>
+        <p>신규기업</p>
+      </div>
+
+      <div className="box">
+        {MainNewPosts.map((post, index) => <Posts key={index} post={post} />)}
+      </div>
+
+      <div className='Posts'>
+        <p>추천기업</p>
+      </div>
+
+      <div className="box">
+        {MainRecommendPosts.map((post, index) => <Posts key={index} post={post} />)}
+      </div>
+    </>
+  );
+}
+
+export default Mainpage;
